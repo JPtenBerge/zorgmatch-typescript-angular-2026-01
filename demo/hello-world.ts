695c02afa65cfa841c0ca9b61dfdc8fb0a102457
JP <jp@jp.nl>
2026-01-26 11:02:30 +0100

    Getting started with TypeScript 🤘

---
+++ demo/hello-world.ts
@@ -0,0 +1,12 @@
+let x: number = 68;
+
+console.log("x:", x);
+
+class Hoi {}
+
+class Bla extends Hoi {}
+
+async function huh() {}
+
+let bla = new Bla();
+console.log(bla);
---
+++ waarom-geen-var.js
@@ -0,0 +1,7 @@
+
+// console.log('i voor for:', i);
+
+for (let i = 0; i < 5; i++) {
+    console.log('i in for:', i);
+}
+// console.log('i buiten for:', i);
