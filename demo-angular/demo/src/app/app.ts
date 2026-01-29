b17ba8970f21908749f923402e9206efde3e6de1
JP <jp@jp.nl>
2026-01-29 11:56:26 +0100

    Backendcommunicatie

--- demo-angular/demo/src/app/app.ts
+++ demo-angular/demo/src/app/app.ts
@@ -1,10 +1,11 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, signal, viewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, inject, input, signal, viewChild } from '@angular/core';
 import { form, FormField, pattern, required } from '@angular/forms/signals';
 import { ValidationMessages } from './validation-message';
 import { Life } from './life';
 import { Autocompleter } from './components/autocompleter/autocompleter';
 import { Framework } from './entities/framework';
+import { HttpClient, httpResource } from '@angular/common/http';
 
 @Component({
 	selector: 'app-root',
@@ -14,6 +15,9 @@ import { Framework } from './entities/framework';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
+	private http = inject(HttpClient);
+	// private cdr = inject(ChangeDetectorRef);
+
 	autocompleter = viewChild(Autocompleter);
 
 	showLife = false;
@@ -43,37 +47,25 @@ export class App {
 		pattern(p.name, /^[A-Z][a-zA-Z]*$/, { message: 'Alleen letters graag. En een hoofdletter aant begin.' });
 	});
 
+	frameworks = signal<Framework[]>([]);
+	id = input.required<number>();  // /product/456
+
+	// GET
+	fancyFrameworks = httpResource<Framework[]>(() => `http://localhost:3000/frameworks/${this.id()}`);
+
+	ngOnInit() {
+		this.http.get<Framework[]>('http://localhost:3000/frameworks').subscribe(fetchedFrameworks => {
+			this.frameworks.update(frameworks => [...frameworks, ...fetchedFrameworks])
+		});
+	}
+
 	addFramework() {
 		this.addFrameworkForm().reset();
 		// this.addFrameworkValue.set({ })
 
 		console.log('werkt!', this.addFrameworkValue());
-		this.frameworks.push({ id: 0, ...this.addFrameworkValue() });
+		// this.frameworks.push({ id: 0, ...this.addFrameworkValue() });
 	}
-
-	frameworks: Framework[] = [
-		{
-			id: 4,
-			name: 'Angular',
-			logoUrl:
-				'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fd585tldpucybw.cloudfront.net%2Fsfimages%2Fdefault-source%2Fblogs%2F2024%2F2024-04%2Fangular-logo.png%3Fsfvrsn%3D543455a3_1&f=1&nofb=1&ipt=ea966f23409b23d3976ff4b26e816da342f74af517995f6e8351f63e6c3456d4',
-			grade: 8.5,
-		},
-		{
-			id: 8,
-			name: 'React',
-			logoUrl:
-				'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F15%2FReact-Logo-PNG.png&f=1&ipt=c9c495c40c55fa88c595c180e4973514c8a305e874f4946c718f3dbdbccce9da',
-			grade: 4,
-		},
-		{
-			id: 15,
-			name: 'Svelte',
-			logoUrl:
-				'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fswiftlet.co.th%2Fwp-content%2Fuploads%2F2022%2F11%2F1200px-Svelte_Logo.svg.png&f=1&nofb=1&ipt=1df9f2771e816475a2ead954b8a2a016454ea7e7e23daddd2097264bc6dcfa63',
-			grade: 9,
-		},
-	];
 
 	changeName() {
 		console.log(setTimeout);
